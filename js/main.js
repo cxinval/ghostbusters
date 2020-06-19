8fa6e091131aa270588eb7bcb0bbbf3648291ca0
cxinval <66485848+cxinval@users.noreply.github.com>
2020-06-19 21:56:32 +0300

    Upload all file

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    const mMenuBtn = $(".m_menu_button");
+    const mMenu = $(".m-menu");
+    const tab = $(".tab");
+
+    mMenuBtn.on("click", function (){
+        mMenu.toggleClass('active');
+        $("body").toggleClass("no-scroll");    
+  });
+
+  tab.on("click", function(){
+      tab.removeClass("active");
+      $(this).toggleClass("active");
+      let activeTabContent = $(this).attr("data-target");
+      $(".tabs_content").removeClass("visible");
+      $(activeTabContent).toggleClass("visible");
+  });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    breakpoints: {
+        992: {
+            slidesPerView: 4,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        320: {
+            slidesPerView: 1,
+            navigation: {
+                nextEl: ".button_next"
+            }
+        }
+    } 
+ 
+  });    
+});
